7e0aa8556d6a3a55b21ce91802c38e8f9faa5d42
Madeline Sharp <35790499+maddiesharp@users.noreply.github.com>
2025-03-15 09:08:13 -0400

    fixed broken test discovery
    Test discovery gets broken if you link the project path to the source's *.obj files. When you link ALL the object files, it replaces gtest's main with the actual applications main, so gtest isn't actually running. You will need to link all the obj files individual or put them in a separate dir from the production application's main obj file.

---
+++ MaddieBoggle-Test/tests/Dictionary_Tests.cpp
@@ -0,0 +1,31 @@
+
+#include "pch.h"
+
+TEST(Dictionary_Tests, MissingFileHandledCorrectly)
+{
+	// Arrange
+	auto errorCode = 0;
+	auto dictionary = Dictionary();
+	string filepath = "this/path/should/not/exist.txt";
+
+	// Act
+	errorCode = dictionary.importDictionary(filepath);
+
+	// Assert (expected, actual)
+	EXPECT_EQ(1, errorCode);
+}
+
+
+/*
+TEST(BoggleBoard_Tests, )
+{
+	// Arrange
+
+
+	// Act
+
+
+	// Assert (expected, actual)
+
+}
+*/
