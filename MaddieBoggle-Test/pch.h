5384a10edb20d37a5acba679400d37118f149abc
Madeline Sharp <35790499+maddiesharp@users.noreply.github.com>
2025-03-14 20:36:22 -0400

    created unit test project

---
+++ MaddieBoggle-Test/pch.h
@@ -0,0 +1,14 @@
+//
+// pch.h
+//
+
+#pragma once
+
+#include "gtest/gtest.h"
+
+#include "BoggleBoard.h"
+#include "BoggleSolver.h"
+#include "BoggleSolverHelper.h"
+#include "Dictionary.h"
+#include "ThreadPool.h"
+
